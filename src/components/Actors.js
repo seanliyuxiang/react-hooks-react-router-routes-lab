f1ec00c5ff8300e9c128886b4e4bdcef1e54a530
Yuxiang Li <seanliyuxiang@gmail.com>
2021-10-01 22:42:32 -0500

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,31 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const actorsListJSX = actors.map(
+    (singleActor, idx) => {
+      return (
+        <div key={idx}>
+          <h4>Name: {singleActor.name}</h4>
+          <p>Movies:</p>
+          <ul>
+            {singleActor.movies.map(
+              (singleMovie, index) => {
+                return <li key={index}>{singleMovie}</li>;
+              }
+            )}
+          </ul>
+        </div>
+      );
+    }
+  );
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorsListJSX}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,32 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const moviesListJSX = movies.map(
+    (singleMovie, idx) => {
+      return (
+        <div key={idx}>
+          <h4>Name: {singleMovie.title}</h4>
+          <p>Time: {singleMovie.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {singleMovie.genres.map(
+              (singleGenre, index) => {
+                return <li key={index}>{singleGenre}</li>;
+              }
+            )}
+          </ul>
+        </div>
+      );
+    }
+  );
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {moviesListJSX}
+    </div>
+  );
 }
 
 export default Movies;
