29fcbea16c02a0fdc13047f779c30633840a3a14
Yuxiang Li <seanliyuxiang@gmail.com>
2021-10-01 22:41:49 -0500

    commit tree 0d77875409e021c932e88cc3bfefebd6d8f52e9b

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,31 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const directorsListJSX = directors.map(
+    (singleDirector, idx) => {
+      return (
+        <div key={idx}>
+          <h4>Name: {singleDirector.name}</h4>
+          <p>Movies:</p>
+          <ul>
+            {singleDirector.movies.map(
+              (singleMovie, index) => {
+                return <li key={index}>{singleMovie}</li>;
+              }
+            )}
+          </ul>
+        </div>
+      );
+    }
+  );
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorsListJSX}
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,30 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <ul className="navbar">
+      <li>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/movies" exact>
+          Movies
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/directors" exact>
+          Directors
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/actors" exact>
+          Actors
+        </NavLink>
+      </li>
+    </ul>
+  );
 }
 
 export default NavBar;
